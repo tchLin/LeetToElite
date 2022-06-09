977bf7296a9d993ecc958ca2c91e09ed3c7375b8
tchLin <106881779+tchLin@users.noreply.github.com>
2022-06-09 19:20:42 -0400

    Update first_unique_character_in_a_string.py

--- solutions/Easy/first_unique_character_in_a_string.py
+++ solutions/Easy/first_unique_character_in_a_string.py
@@ -1,5 +1,17 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
+        char_dict={}
+        for i in s:
+            char_dict[i]=char_dict.get(i, 0)+1
+        
+        for char in char_dict:
+            if char_dict[char] == 1:
+                return s.index(char)
+        return -1
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
         check_dict={}
         for char in s:
             if char not in check_dict:
