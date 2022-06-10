beb9ef5f25500b91b9517c086b2f204f7357fdd5
tchLin <106881779+tchLin@users.noreply.github.com>
2022-06-09 20:08:06 -0400

    Update two_sum.py

--- solutions/Easy/two_sum.py
+++ solutions/Easy/two_sum.py
@@ -1,5 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict={}
+        for i, num in enumerate(nums):
+            if target-num in num_dict:
+                return [num_dict[target-num], i]
+            num_dict[num]=num_dict.get(num, i)
+            
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         prevtable={}
         for i, num in enumerate(nums):
             key = target - num
